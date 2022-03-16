c4f44c0eea7e370b939d49cbeeb24b65c453c636
AjayDhanwade <101167434+AjayDhanwade@users.noreply.github.com>
2022-03-16 18:00:36 +0530

    Add files via upload

---
+++ reversed.py
@@ -0,0 +1,7 @@
+# mirror image of string 
+
+string=input()         # by default input is in the string
+str_rev=" "   # revese using string format
+for i in string:
+    str_rev=i +str_rev
+print(str_rev)    
